bb9946df61e83eb495dd7f18bef9027c8c4baeae
roimahns <109640106+roimahns@users.noreply.github.com>
2022-10-09 10:58:59 -0400

    added button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ function App() {
       <i className = "creater">Made By: Roi Mahns</i>
       <br></br><br></br><br></br><br></br><br></br><br></br>
       <a class="button">PLAY</a>
-      <image>https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dc5e9c50-a07d-40fa-9e0d-9f5c7f4378f0/db8l6ab-e137a10e-a718-41dd-ba0c-f4537e40d0b5.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RjNWU5YzUwLWEwN2QtNDBmYS05ZTBkLTlmNWM3ZjQzNzhmMFwvZGI4bDZhYi1lMTM3YTEwZS1hNzE4LTQxZGQtYmEwYy1mNDUzN2U0MGQwYjUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0._bYretKmnKmuxv7Zg-GkHck6YJJmoq5yRv9vChqqB8w</image>
     </div>
   
   
