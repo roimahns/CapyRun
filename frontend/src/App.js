30629850e8f672c8830239f293b5f8d72ada174a
roimahns <109640106+roimahns@users.noreply.github.com>
2022-10-16 13:44:17 -0400

    Update App.js
    deleted bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,10 @@ function App() {
     <div className="App">
       <h1 className = "Title">CapyRun!</h1>
       <i className = "creater">Made By: Roi Mahns</i>
-      <br></br><br></br><br></br><br></br><br></br><br></br>
+      <br></br><br></br><br></br><br></br><br></br>
       <a class="button">PLAY</a>
       <img src="./pinetree" className = "pinetree" alt = "pinetree"></img>
+
     </div>
   
   
