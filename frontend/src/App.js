4cecd8f2a254514552fb0e110ca8ab12d534a08c
roimahns <109640106+roimahns@users.noreply.github.com>
2022-10-16 15:03:40 -0400

    Fixed image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
+import pinetree from "./pinetree2.jpg";
 function App() {
   return (
     <div className="App">
-      <h1 className = "Title">CapyRun!</h1>
-      <i className = "creater">Made By: Roi Mahns</i>
+      <h1 className="Title">CapyRun!</h1>
+      <i className="creater">Made By: Roi Mahns</i>
+
+
       <br></br><br></br><br></br><br></br><br></br>
-      <a class="button">PLAY</a>
-      <img src="./pinetree" className = "pinetree" alt = "pinetree"></img>
+
+
+      <button class="button">PLAY</button>
+      <div> <img src={pinetree} className="pinetree2" alt = "pinetree"></img>
+      </div>
 
     </div>
-  
-  
+
+
   );
 }
 
