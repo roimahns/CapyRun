61f0e17dcf7980faed5c8c8e6b04a08490e12c97
roimahns <109640106+roimahns@users.noreply.github.com>
2022-10-29 10:49:30 -0400

    Fixed
    Fixed problems

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 import Board from "./Board";
-import pinetree from "./pinetree2.jpg";
 function App() {
   return (
     <div className="backround">
       <h1 className="Title">CapyRun!</h1>
       <i className="creater">Made By: Roi Mahns</i>
-      <br></br><br></br>
-      <h6 className="transparent">i</h6>
 
 
       <br></br><br></br>
